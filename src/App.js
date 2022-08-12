93cb3267952bb3c40f8f0ba664396fb5ba4ab768
harry-private <isitharry2@gmail.com>
2022-08-12 12:37:01 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import Table from './Table'
+// import Form from './Form'
+// import Api from './Api'
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /*   state = {
+      characters: []
+    }
+  
+    removeCharacter = (index) => {
+      const { characters } = this.state
+  
+      this.setState({
+        characters: characters.filter((character, i) => {
+          return i !== index
+        }),
+      })
+    }
+  
+    handleSubmit = (character) => {
+      this.setState({
+        characters: [...this.state.characters, character]
+      })
+    }
+  
+    render() {
+      const { characters } = this.state
+  
+      return (
+        <div className="container">
+          <Table characterData={characters} removeCharacter={this.removeCharacter} />
+          <Form handleSubmit={this.handleSubmit} />
+        </div>
+      )
+    } */
+
+
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*"
+
+
+
+    fetch(url)
+      .then((result) => result.json())
+      .then((result) => {
+        this.setState({
+          data: result
+        })
+      })
+  }
+
+  render() {
+    const { data } = this.state
+
+    const result = data.map((entry, index) => {
+      return <li key={index}>{entry}</li>
+    })
+    return <ul>{result}</ul>
+  }
+
+
 }
 
-export default App;
+export default App
